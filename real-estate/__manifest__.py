2f3dc693a3400f8d13276cde456e4493a63e96a1
ahmed youssef <ayopopa36@gmail.com>
2022-08-16 17:49:59 +0200

    Add files via upload

---
+++ real-estate/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "real-estate",
+
+    'description': "real estate management system",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'sequence': -100,
+
+    'category': 'real_estate',
+    'version': '0.1',
+    'application': 'Ture',
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+
+        'data/data.xml',
+
+        'views/estate_propirty_offers.xml',
+        'views/estate_propirty_tags.xml',
+        'views/estate_propirty_type.xml',
+        'views/estate_propirty.xml',
+        'views/menu.xml',
+
+
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
